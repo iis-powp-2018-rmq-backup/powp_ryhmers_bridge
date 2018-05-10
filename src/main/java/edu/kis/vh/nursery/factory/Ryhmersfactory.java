ee19272a016d01f9b29de85abc0c67848e2f4a20
Guciio <gucio332@gmail.com>
2018-05-10 14:46:05 +0200

    fixed bad indents in code.

--- src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
@@ -2,14 +2,14 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRyhmer;
 
-	public interface Ryhmersfactory {
-	
-		public defaultCountingOutRyhmer GetStandardRyhmer();
-		
-		public defaultCountingOutRyhmer GetFalseRyhmer();
-		
-		public defaultCountingOutRyhmer GetFIFORyhmer();
-		
-		public defaultCountingOutRyhmer GetHanoiRyhmer();
-		
-	}
+public interface Ryhmersfactory {
+
+	public defaultCountingOutRyhmer GetStandardRyhmer();
+
+	public defaultCountingOutRyhmer GetFalseRyhmer();
+
+	public defaultCountingOutRyhmer GetFIFORyhmer();
+
+	public defaultCountingOutRyhmer GetHanoiRyhmer();
+
+}
