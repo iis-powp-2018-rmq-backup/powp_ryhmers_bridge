485125b67c91513e4ae2d533f7c4a2785aaffb40
Mayar Karkout <204641@edu.p.lodz.pl>
2018-05-10 13:42:06 +0200

    3.1.1, 3.1.2: lines number 5, 7-9, 11, 13-15

--- src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
@@ -7,29 +7,33 @@ import edu.kis.vh.nursery.factory.Ryhmersfactory;
 
 class RyhmersDemo {
 
-	public static void main(String[] args) {
-		Ryhmersfactory factory = new DefaultRyhmersFactory();
-		
-		defaultCountingOutRyhmer[] ryhmers = { factory.GetStandardRyhmer(), factory.GetFalseRyhmer(),
-				factory.GetFIFORyhmer(), factory.GetHanoiRyhmer()};
-		
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
-				ryhmers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < 15; i++)
-			ryhmers[3].countIn(rn.nextInt(20));
-		
-		for (int i = 0; i < ryhmers.length; i++) {
-			while (!ryhmers[i].callCheck())
-				System.out.print(ryhmers[i].countOut() + "  ");
-			System.out.println();
-		}
-		
-		System.out.println("total rejected is "
-				+ ((HanoiRyhmer) ryhmers[3]).reportRejected());
-		
-	}
-	
+    public static void main(String[] args) {
+        Ryhmersfactory factory = new DefaultRyhmersFactory();
+
+        defaultCountingOutRyhmer[] ryhmers = {factory.GetStandardRyhmer(), factory.GetFalseRyhmer(),
+            factory.GetFIFORyhmer(), factory.GetHanoiRyhmer()};
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 0; j < 3; j++) {
+                ryhmers[j].countIn(i);
+            }
+        }
+
+        java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < 15; i++) {
+            ryhmers[3].countIn(rn.nextInt(20));
+        }
+
+        for (int i = 0; i < ryhmers.length; i++) {
+            while (!ryhmers[i].callCheck()) {
+                System.out.print(ryhmers[i].countOut() + "  ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("total rejected is "
+                + ((HanoiRyhmer) ryhmers[3]).reportRejected());
+
+    }
+
 }
