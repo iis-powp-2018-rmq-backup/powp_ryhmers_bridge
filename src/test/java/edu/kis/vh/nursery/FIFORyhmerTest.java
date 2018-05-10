62f529221a757a8d75a573b27301b555abbdb6c5
Klaudia Goczal <klaudia@lodz.pl>
2018-05-11 00:27:47 +0200

    3.2.2 Added FIFORyhmer tests

---
+++ src/test/java/edu/kis/vh/nursery/FIFORyhmerTest.java
@@ -0,0 +1,39 @@
+package edu.kis.vh.nursery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FIFORyhmerTest {
+
+	private FIFORyhmer ryhmer = new FIFORyhmer();
+
+	@Test
+	public void testCountOutReturnsMinus1WhenRyhmersEmpty() {
+		int EMPTY_STACK_VALUE = -1;
+		int result = ryhmer.countOut();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+	}
+
+	@Test
+	public void testCountOutReturnsValueWhenOneAdded() {
+		int testValue = 88;
+		ryhmer.countIn(testValue);
+		int result = ryhmer.countOut();
+		Assert.assertEquals(testValue, result);
+	}
+
+	@Test
+	public void testCountOutReturnsValuesFIFOWhenFiveAdded() {
+		int startingValue = 88;
+		for(int i = 0; i < 5; i++){
+			int testValue = startingValue + i;
+			ryhmer.countIn(testValue);
+		}
+
+		for(int i = 0; i < 5; i++) {
+			int testValue = startingValue + i;
+			int result = ryhmer.countOut();
+			Assert.assertEquals(testValue, result);
+		}
+	}
+}
