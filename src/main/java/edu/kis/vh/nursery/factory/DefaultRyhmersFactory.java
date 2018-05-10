536a4adef7c3ee0b28471b2826ce551baaacccf0
Monika <jedrzejekmonika@gmail.com>
2018-05-10 13:14:40 +0200

    3.1.1 Row nr 5, 10, 15

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRyhmersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRyhmersFactory.java
@@ -1,30 +1,29 @@
 package edu.kis.vh.nursery.factory;
 
-import edu.kis.vh.nursery.defaultCountingOutRyhmer;
 import edu.kis.vh.nursery.FIFORyhmer;
 import edu.kis.vh.nursery.HanoiRyhmer;
-import edu.kis.vh.nursery.factory.Ryhmersfactory;
+import edu.kis.vh.nursery.defaultCountingOutRyhmer;
 
 public class DefaultRyhmersFactory implements Ryhmersfactory {
 
-	@Override
-	public defaultCountingOutRyhmer GetStandardRyhmer() {
-		return new defaultCountingOutRyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetStandardRyhmer() {
+        return new defaultCountingOutRyhmer();
+    }
 
-	@Override
-	public defaultCountingOutRyhmer GetFalseRyhmer() {
-		return new defaultCountingOutRyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetFalseRyhmer() {
+        return new defaultCountingOutRyhmer();
+    }
 
-	@Override
-	public defaultCountingOutRyhmer GetFIFORyhmer() {
-		return new FIFORyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetFIFORyhmer() {
+        return new FIFORyhmer();
+    }
 
-	@Override
-	public defaultCountingOutRyhmer GetHanoiRyhmer() {
-		return new HanoiRyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetHanoiRyhmer() {
+        return new HanoiRyhmer();
+    }
 
 }
