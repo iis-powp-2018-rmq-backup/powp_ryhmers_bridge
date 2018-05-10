ce6d133e7753ea57f6593f74c085ddb6ba025784
bartwe2 <barwer2@gmail.com>
2018-05-10 14:53:23 +0200

    fixed code in factory folder

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRyhmersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRyhmersFactory.java
@@ -7,24 +7,24 @@ import edu.kis.vh.nursery.factory.Ryhmersfactory;
 
 public class DefaultRyhmersFactory implements Ryhmersfactory {
 
-	@Override
-	public defaultCountingOutRyhmer GetStandardRyhmer() {
-		return new defaultCountingOutRyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetStandardRyhmer() {
+        return new defaultCountingOutRyhmer();
+    }
 
-	@Override
-	public defaultCountingOutRyhmer GetFalseRyhmer() {
-		return new defaultCountingOutRyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetFalseRyhmer() {
+        return new defaultCountingOutRyhmer();
+    }
 
-	@Override
-	public defaultCountingOutRyhmer GetFIFORyhmer() {
-		return new FIFORyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetFIFORyhmer() {
+        return new FIFORyhmer();
+    }
 
-	@Override
-	public defaultCountingOutRyhmer GetHanoiRyhmer() {
-		return new HanoiRyhmer();
-	}
+    @Override
+    public defaultCountingOutRyhmer GetHanoiRyhmer() {
+        return new HanoiRyhmer();
+    }
 
 }
