bd1be94f812ae3c03f9ccccd3845a64af0887e8a
adinowi <adinowi@gmail.com>
2018-05-10 12:50:47 +0200

    poprawiono formatowanie, klasa HanoiRyhmer bledne wciecia w wierszach: 5 12 14 15

--- src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
@@ -2,14 +2,14 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRyhmer;
 
-	public interface Ryhmersfactory {
+public interface Ryhmersfactory {
+
+	public defaultCountingOutRyhmer GetStandardRyhmer();
 	
-		public defaultCountingOutRyhmer GetStandardRyhmer();
-		
-		public defaultCountingOutRyhmer GetFalseRyhmer();
-		
-		public defaultCountingOutRyhmer GetFIFORyhmer();
-		
-		public defaultCountingOutRyhmer GetHanoiRyhmer();
-		
-	}
+	public defaultCountingOutRyhmer GetFalseRyhmer();
+	
+	public defaultCountingOutRyhmer GetFIFORyhmer();
+	
+	public defaultCountingOutRyhmer GetHanoiRyhmer();
+	
+}
