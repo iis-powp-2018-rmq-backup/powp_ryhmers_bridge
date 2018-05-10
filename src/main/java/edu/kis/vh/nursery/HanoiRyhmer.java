627ffc3e2423e55f7fc0a7f674b9a643bed71ffb
Marcin Kapelan <marcinkape@gmail.com>
2018-05-10 14:58:43 +0200

    Use IDE automatic formatting

--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -4,14 +4,14 @@ public class HanoiRyhmer extends defaultCountingOutRyhmer {
 
     int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	    if (!callCheck() && in > peekaboo())
-			totalRejected++;
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
         else
             super.countIn(in);
-	}
+    }
 }
