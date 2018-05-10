a568d29ec695a07fa2d1564aa20c62f2f4a48395
AgheroLab <AgheroLab@mail.com>
2018-05-10 13:36:38 +0200

    Poprawa modyfikatorow dostepu

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,11 +2,11 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int INT = -1;
+    private static final int INT = -1;
     Node last;
     int i;
 
-    public void push(int i) {
+    private void push(int i) {
         if (last == null)
             last = new Node(i);
         else {
@@ -16,21 +16,21 @@ public class IntLinkedList {
         }
     }
 
-    public boolean isEmpty() {
+    private boolean isEmpty() {
         return last == null;
     }
 
-    public boolean isFull() {
+    private boolean isFull() {
         return false;
     }
 
-    public int top() {
+    private int top() {
         if (isEmpty())
             return INT;
         return last.value;
     }
 
-    public int pop() {
+    private int pop() {
         if (isEmpty())
             return INT;
         int ret = last.value;
