966a346b59b811a2747a30ae99fe41d26a301cf0
moody16 <jan.bargiel96@gmail.com>
2018-05-10 13:05:19 +0200

    Poprawienie bledow formatowanie pozostalych klas

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRyhmer.java
@@ -7,28 +7,28 @@ public class defaultCountingOutRyhmer {
 	public int total = -1;
 
 	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
+        if (!isFull())
+            NUMBERS[++total] = in;
 	}
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+    public boolean callCheck() {
+        return total == -1;
+    }
+
+    public boolean isFull() {
+        return total == 11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return -1;
+        return NUMBERS[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return -1;
+        return NUMBERS[total--];
+    }
 
 }
--- src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
@@ -2,14 +2,14 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRyhmer;
 
-	public interface Ryhmersfactory {
-	
-		public defaultCountingOutRyhmer GetStandardRyhmer();
-		
-		public defaultCountingOutRyhmer GetFalseRyhmer();
-		
-		public defaultCountingOutRyhmer GetFIFORyhmer();
-		
-		public defaultCountingOutRyhmer GetHanoiRyhmer();
-		
-	}
+public interface Ryhmersfactory {
+
+    defaultCountingOutRyhmer GetStandardRyhmer();
+
+    defaultCountingOutRyhmer GetFalseRyhmer();
+
+    defaultCountingOutRyhmer GetFIFORyhmer();
+
+    defaultCountingOutRyhmer GetHanoiRyhmer();
+
+}
