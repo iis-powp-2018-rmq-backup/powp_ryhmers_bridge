29acb39ffc8d03c01ee22072a43d3ae907b9637c
bartwe2 <barwer2@gmail.com>
2018-05-10 15:05:28 +0200

    fixed name convention and typos

---
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -0,0 +1,23 @@
+package edu.kis.vh.nursery;
+
+public class HanoiRhymer extends DefaultCountingOutRhymer {
+
+    int totalRejected = 0;
+
+    public int reportRejected() {
+        return totalRejected;
+    }
+
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
+
+    /*W intelij:
+        alt + ctrl + strzałka, przenosi miedzy ostatnimi pozycjami kursorów
+        alt + shitf + backspace, przenosi w miejsce ostatniej edycji
+
+     */
+}
