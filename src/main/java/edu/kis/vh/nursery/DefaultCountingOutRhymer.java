4a46175a63b3fcec8d38abc0b9aea77e0de52545
Rafał Kusy <rafikus95@gmail.com>
2018-05-10 15:29:49 +0200

    3.1.6 Zminimalizowano widoczność metod i atrybutów
    Oraz poprawki do 3.1.5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,22 +4,22 @@ public class DefaultCountingOutRhymer {
 
 	private static int NUMBERS_CAPACITY = 12;
 
-	private static int STARTING_CAPACITY = -1;
+	private static int EMPTY_STACK_VALUE = -1;
 
 	private int[] numbers = new int[NUMBERS_CAPACITY];
 
-	public int total = STARTING_CAPACITY;
+	private int total = EMPTY_STACK_VALUE;
 
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
-	public boolean callCheck() {
-		return total == STARTING_CAPACITY;
+	protected boolean callCheck() {
+		return total == EMPTY_STACK_VALUE;
 	}
 
-	public boolean isFull() {
+	protected boolean isFull() {
 		return total == NUMBERS_CAPACITY - 1;
 	}
 
