4e3d9b4c7df1f1f6862dc8013a20fa5259ad2c3e
Kamil Prus <kamil230596@gmail.com>
2018-05-10 14:47:54 +0200

    Note how alt + left/right arrow key affect in IDE

--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.nursery;
 
+/**
+ * alt + strzałka lewo/prawo ustawia kursor na podstawie jego historii położenia.
+ */
+
+
 public class HanoiRyhmer extends defaultCountingOutRyhmer {
 
 	int totalRejected = 0;
