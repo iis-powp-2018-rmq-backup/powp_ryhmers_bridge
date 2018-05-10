1f2de0a53aaf770a2cf0482c2d1a009cdb096879
ganymede95 <195060@edu.p.lodz.pl>
2018-05-10 14:51:27 +0200

    Step 3.1.3: Explanation for alt + <- / alt + -> shortcuts

--- src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
@@ -11,5 +11,7 @@ public interface Ryhmersfactory {
 	public defaultCountingOutRyhmer GetFIFORyhmer();
 	
 	public defaultCountingOutRyhmer GetHanoiRyhmer();
-
+	// alt + <- / alt + -> allows the user to switch between classes in the IDE opened by the user
+	//(alt + -> - switch to next file opened after the current one, 
+	// alt + <- - switch to previous file opened before the current one
 }
