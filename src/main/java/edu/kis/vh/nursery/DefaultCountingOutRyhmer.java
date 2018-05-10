140fb77dc0fbd41d98b81e4fc3f9a1112d223ee6
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-05-10 15:27:44 +0200

    refraktoryzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRyhmer {
 
-	private int[] numbers = new int[12];
+	private static final int ARRAY_EMPTY = -1;
 
-	public int total = -1;
+	private static final int NUMBER_OF_NUMBERS = 12;
+
+	private int[] numbers = new int[NUMBER_OF_NUMBERS];
+
+	public int total = ARRAY_EMPTY;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,7 +16,7 @@ public class DefaultCountingOutRyhmer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == ARRAY_EMPTY;
 	}
 
 	public boolean isFull() {
@@ -21,13 +25,13 @@ public class DefaultCountingOutRyhmer {
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return ARRAY_EMPTY;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return ARRAY_EMPTY;
 		return numbers[total--];
 	}
 
