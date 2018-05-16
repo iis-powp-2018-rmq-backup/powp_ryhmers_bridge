24d49248eabd1e873ae0a01fcc8b6f68e3363c62
adinowi <adinowi@gmail.com>
2018-05-16 21:26:29 +0200

    dodana dokumentacja do HanoiRyhmer

--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -1,13 +1,25 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Wyliczanka Hanoi
+ *
+ */
 public class HanoiRyhmer extends DefaultCountingOutRyhmer {
 
 	private int totalRejected = 0;
 
+	/**
+	 * Zwraca ilość nieprzyjętych liczb do wyliczanki
+	 * @return ilość nieprzyjętych liczb do wyliczanki
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * Umieszcza podaną wartość w wyliczance(nie jest możliwe zgłoszenie liczby większej niż bieżaca)
+	 * @param wartość liczby do umieszczenia w wyliczance
+	 */
 	@Override
 	public void countIn(final int in) {
 		if (!callCheck() && in > peekaboo())
