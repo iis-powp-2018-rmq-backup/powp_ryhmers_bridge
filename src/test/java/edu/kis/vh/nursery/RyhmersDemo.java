41b8d40735dce57cb95ba0a7c20aa3013b9dce33
Piotr Sadza <zizi5673@gmail.com>
2018-05-17 00:53:56 +0200

    3.1.13

--- src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
@@ -9,27 +9,32 @@ class RyhmersDemo {
 
 	public static void main(String[] args) {
 		Ryhmersfactory factory = new DefaultRyhmersFactory();
-		
+
+		testRhymers(factory);
+
+	}
+
+	private static void testRhymers(Ryhmersfactory factory) {
 		DefaultCountingOutRyhmer[] ryhmers = { factory.getStandardRyhmer(), factory.getFalseRyhmer(),
 				factory.getFIFORyhmer(), factory.getHanoiRyhmer()};
-		
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				ryhmers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			ryhmers[3].countIn(rn.nextInt(20));
-		
+
 		for (int i = 0; i < ryhmers.length; i++) {
 			while (!ryhmers[i].callCheck())
 				System.out.print(ryhmers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRyhmer) ryhmers[3]).reportRejected());
-		
 	}
-	
+
+
 }
