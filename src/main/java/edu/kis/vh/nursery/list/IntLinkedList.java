9214081a09507ecdfba19c3a26ff8f096424b63a
rafal <rafal@jakubowski>
2018-05-17 00:49:36 +0200

    Node is one of many elements in the list, so it can be private inside the IntLinkedList class.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,4 +38,43 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	private class Node {
+
+		private int value;
+		/*
+		 * W klasie IntLinkedList zmieniło się z bezpośredniego odwołania do tego atrybutu
+		 * na pośrednie, przez getValue()
+		 */
+		private Node prev;
+		private Node next;
+		/*
+		 * To samo co wyżej, dodatkowo do przypisania wartości zostało wykorzystane 
+		 * setPrev() i setNext()
+		 */
+
+		public Node(int i) {
+			value = i;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+
+	}
 }
