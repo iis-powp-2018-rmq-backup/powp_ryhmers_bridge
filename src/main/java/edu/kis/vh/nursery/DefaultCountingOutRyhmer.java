5b19f8e428f0fe8063d5966eb62332301ff5d273
rafal <rafal@jakubowski>
2018-05-16 21:19:38 +0200

    changed field visibility

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
@@ -8,7 +8,7 @@ public class DefaultCountingOutRyhmer {
 
 	private int[] numbers = new int[CAPACITY];
 
-	public int total = EMPTY_ARRAY;
+	private int total = EMPTY_ARRAY;
 
 	public void countIn(int in) {
 		if (!isFull())
