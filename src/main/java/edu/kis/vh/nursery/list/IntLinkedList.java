90de612658963d1294b1b9a9e9bb8a1cee266f36
Piotr Sadza <zizi5673@gmail.com>
2018-05-17 00:39:51 +0200

    3.1.12 Changed node public class to a nested class for IntLinkedList class, because it is the only class which uses node classes.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -55,3 +55,34 @@ public class IntLinkedList {
 	}
 
 }
+
+class Node {
+
+	private final int value;
+	private Node prev, next;
+
+	public Node(final int i) {
+		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+}
