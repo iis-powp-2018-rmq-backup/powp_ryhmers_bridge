0ff43b913497042c1cd1c772cc4d37687484bb98
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-05-16 20:03:34 +0200

    Zadanie 8. Utworzone gettery/settery zastapily odwolania w innych klasach

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,12 +1,38 @@
 package edu.kis.vh.nursery.list;
 
 public class Node {
-
-	int value;
-	Node prev, next;
+// dla poniższych pol refactor spowodował utworzenie setterow/getterów i ic automatyczne użucie w miejscu bezposredniego 
+	//odwolania do pol
+	private int value;
+	private Node prev;
+	private Node next;
 
 	public Node(int i) {
 		value = i;
+	}
+
+	int getValue() {
+		return value;
+	}
+
+	void setValue(int value) {
+		this.value = value;
+	}
+
+	Node getPrev() {
+		return prev;
+	}
+
+	void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	Node getNext() {
+		return next;
+	}
+
+	void setNext(Node next) {
+		this.next = next;
 	}
 
 }
