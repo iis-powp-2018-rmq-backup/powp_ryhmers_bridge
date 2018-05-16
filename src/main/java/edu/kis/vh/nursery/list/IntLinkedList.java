88acf234ff961caba0f57b131e1c0bfedbc36921
Marcin Kapelan <marcinkape@gmail.com>
2018-05-16 23:52:05 +0200

    Move Node class to IntLinkedList and fix mistake in my comment from previous commit -Automatic encapsulation caused IntLinkedList class' methods to use generated getters and setters, not Node class of course.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,4 +38,37 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	public class Node {
+
+        private int value;
+        private Node prev;
+        private Node next;
+
+        Node(final int i) {
+            value = i;
+        }
+
+        //Automatic encapsulation caused IntLinkedList class' methods to use getters and setters generated below.
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(final Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(final Node next) {
+            this.next = next;
+        }
+    }
+
 }
