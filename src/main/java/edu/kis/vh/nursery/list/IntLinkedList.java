f9707cebd802fd024827e256886152f35fdc3c61
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-05-16 20:56:47 +0200

    Zadanie 3.3

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,19 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * @author ola List for handling int values
+ */
 public class IntLinkedList {
 
 	private static final int EMPTY_VALUE = -1;
 	private Node last;
 
+	/**
+	 * Insert value to the list
+	 * 
+	 * @param i
+	 *            value pushed to the list
+	 */
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -15,20 +24,40 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * Check if list is empty
+	 * 
+	 * @return true if empty list, otherwise false
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * Unlimited size, always true
+	 * 
+	 * @return always true
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * return last added value
+	 * 
+	 * @return int value from top
+	 */
 	public int top() {
 		if (isEmpty())
 			return EMPTY_VALUE;
 		return last.getValue();
 	}
 
+	/**
+	 * retun last added value and remove from list
+	 * 
+	 * @return last added value
+	 */
 	public int pop() {
 		if (isEmpty())
 			return EMPTY_VALUE;
@@ -39,6 +68,12 @@ public class IntLinkedList {
 
 }
 
+/**
+ * represetation of single node for storing int value
+ * 
+ * @author ola
+ *
+ */
 class Node {
 	// dla poniższych pol refactor spowodował utworzenie setterow/getterów i ic
 	// automatyczne użucie w miejscu bezposredniego
