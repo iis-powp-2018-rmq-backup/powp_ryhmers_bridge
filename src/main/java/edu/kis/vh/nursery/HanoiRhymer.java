e7b2479a86e8f73077c4bba4c4b0b5d62691167c
[Roksana_Traczyk] <[zakrencona15@wp.pl]>
2018-05-17 00:58:19 +0200

    Create documentation for HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,20 +1,33 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Class is which responsible for total rejected, extends DefaultCountingOutRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
+    /**
+     * Private variable responsible for total rejected
+     */
+    private int totalRejected = 0;
 
-	private int totalRejected = 0;
+    /**
+     * Public metod which return total rejected
+     * @return totalRejected
+     */
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public int reportRejected() {
-		return totalRejected;
-	}
-
-	@Override
-	public void countIn(int in) {
-		if (!callCheck() && in > peekaboo()) {
-			totalRejected++;
-		} else {
-			super.countIn(in);
-		}
-	}
+    /**
+     * Override method which count total rejected
+     * @param in int variable
+     */
+    @Override
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo()) {
+            totalRejected++;
+        } else {
+            super.countIn(in);
+        }
+    }
 }
 //Kombinacja klawiszy alt+<- lub alt+-> : zmienia położenie kursora na jego poprzednie położenie
