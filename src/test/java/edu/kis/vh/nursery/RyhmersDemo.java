2291f2e6cf60061fe725f20faf8a90aa45c512a6
Aghero <tsaghero@gmail.com>
2018-05-16 20:51:29 +0200

    3.13 RyhmersDemo uporządkowanie metody main

--- src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
@@ -10,24 +10,28 @@ class RyhmersDemo {
 		
 		DefaultCountingOutRhymer[] ryhmers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
+
+		testRyhmers(ryhmers);
 		
+	}
+
+	private static void testRyhmers(DefaultCountingOutRhymer[] ryhmers) {
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				ryhmers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			ryhmers[3].countIn(rn.nextInt(20));
-		
+
 		for (int i = 0; i < ryhmers.length; i++) {
 			while (!ryhmers[i].callCheck())
 				System.out.print(ryhmers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) ryhmers[3]).reportRejected());
-		
 	}
-	
+
 }
