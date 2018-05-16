f7e9a49c95b0e89e21f19853725b7064ef91dccf
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-05-16 20:00:22 +0200

    Dodano dokumentację klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,12 +1,26 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * @author Kamil Błażejczuk
+ * Klasa zapewnia strukturę LinkedList tylko dla elementów typu int
+ */
 public class IntLinkedList {
 
-	private static final int DEFAULT_EMPTY_STACK_VALUE = -1;
+	/**
+	 * Domyślna wartość zwracana, gdy lista jest pusta
+	 */
+	private static final int DEFAULT_EMPTY_LIST_VALUE = -1;
 	
+	/**
+	 * Ostatni element listy
+	 */
 	private Node last;
 	private int i;
 
+	/**
+	 * Metoda dodaje nowy element do listy
+	 * @param i Wartość dodanego elementu
+	 */
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -17,23 +31,39 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * Metoda sprawdza czy lista jest pusta
+	 * @return Zwraca true, gdy jest pusta, false, jeśli nie jest
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * Metoda sprawdza czy lista jest pełna
+	 * @return Zawsze zwraca false (lista nie ma limitu pojemności)
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * Metoda zwraca ostatni element z listy
+	 * @return Zwraca ostatni element z listy gdy nie jest pusta, w przeciwnym wypadku zwraca -1
+	 */
 	public int top() {
 		if (isEmpty())
-			return DEFAULT_EMPTY_STACK_VALUE;
+			return DEFAULT_EMPTY_LIST_VALUE;
 		return last.getValue();
 	}
 
+	/**
+	 * Metoda zwraca ostatni element z listy i usuwa go z niej
+	 * @return Zwraca ostatni element z listy gdy nie jest pusta, w przeciwnym wypadku zwraca -1
+	 */
 	public int pop() {
 		if (isEmpty())
-			return DEFAULT_EMPTY_STACK_VALUE;
+			return DEFAULT_EMPTY_LIST_VALUE;
 		final int ret = last.getValue();
 		last = last.getPrev();
 		return ret;
@@ -47,13 +77,26 @@ public class IntLinkedList {
 		return i;
 	}
 	
+	/**
+	 * Klasa zagnieżdżona, zapewniająca klasie IntLinkedList strukturę elemntu listy
+	 *
+	 */
 	private class Node {
 
 		// metody top i pop klasy IntLinkedList używają settera i gettera zamiast bezpośredniego dostępu do pola
+		/**
+		 * Wartość elementu listy
+		 */
 		private int value;
 		// metody push i pop klasy IntLinkedList używają settera i gettera zamiast bezpośredniego dostępu do pola
+		/**
+		 * Poprzedni element listy
+		 */
 		private Node prev;
 		// metoda push klasy IntLinkedList używa settera i gettera zamiast bezpośredniego dostępu do pola
+		/**
+		 * Następny element listy
+		 */
 		private Node next;
 
 		private Node(int i) {
