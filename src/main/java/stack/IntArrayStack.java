edd97069287453bb211db04199391ca3765f2010
rafal <rafal@jakubowski>
2018-05-17 12:36:23 +0200

    moved implementation from DefaultCountingOutRyhmer

---
+++ src/main/java/stack/IntArrayStack.java
@@ -0,0 +1,48 @@
+package stack;
+
+public class IntArrayStack {
+	
+	private static final int EMPTY_ARRAY = -1;
+
+	private static final int CAPACITY = 12;
+
+	private final int[] numbers = new int[CAPACITY];
+
+	private int total = EMPTY_ARRAY;
+
+	public int getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param in any number
+	 */
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	public boolean callCheck() {
+		return total == EMPTY_ARRAY;
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return EMPTY_ARRAY;
+		return numbers[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return EMPTY_ARRAY;
+		return numbers[total--];
+	}
+/*
+ * alt + left arrow = move back to previously modified file 
+ * alt + right arrow = move forward to lately modified file
+ */
+}
