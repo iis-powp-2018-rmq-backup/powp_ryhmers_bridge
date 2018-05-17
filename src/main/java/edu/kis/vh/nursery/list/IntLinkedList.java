8fbe8fc5a7c06d144d6e25974fb500026157267d
Cezary Witkowski <czarecoo@gmail.com>
2018-05-17 14:43:04 +0200

    3.3.2 added documentation sketch to IntLinkedList class

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,26 @@
 package edu.kis.vh.nursery.list;
 
+ /**
+ * Class represents stack as linked list of int values and methods to work with it
+ * @author Cezary Witkowski
+ *
+ */
+
 public class IntLinkedList {
 
+	/**
+	 * Constant Value, which represents empty list
+	 */
     private static final int DEFAULT_EMPTY_STACK_VALUE = -1;
+    /**
+	 * Last element of list
+	 */
     Node last;
-
+    
+    /**
+	 * Method that adds value to the list
+	 * @param i value which will be added to list
+	 */
     public void push(final int i) {
         if (last == null) {
             last = new Node(i);
@@ -15,14 +31,26 @@ public class IntLinkedList {
         }
     }
 
+    /**
+	 * Method checks, if the list is empty
+	 * @return boolean value true or false
+	 */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+	 * Method checks, if the list is full
+	 * @return boolean value true or false
+	 */
     public boolean isFull() {
         return false;
     }
 
+    /**
+	 * Method returns the value of the last added element
+	 * @return integer value of the last added element
+	 */
     public int top() {
         if (isEmpty()) {
             return DEFAULT_EMPTY_STACK_VALUE;
@@ -30,6 +58,10 @@ public class IntLinkedList {
         return last.getValue();
     }
 
+    /**
+	 * Method returns the value of the last added element and removes it from list
+	 * @return integer value of the last added element or the constant value, if case of empty list
+	 */
     public int pop() {
         if (isEmpty()) {
             return DEFAULT_EMPTY_STACK_VALUE;
@@ -41,29 +73,53 @@ public class IntLinkedList {
     
     public class Node {
 
+    	/**
+    	 * Value of node
+    	 */
         private final int value;
+        /**
+    	 * Reference to previous and next node on the list
+    	 */
         private Node prev, next;
-
+        /**
+    	 * Constructor
+    	 * @param i value of the node
+    	 */
         public Node(final int i) {
             value = i;
         }
-
+        /**
+    	 * Getter of the value
+    	 * @return node value
+    	 */
         public int getValue() {
             return value;
         }
-
+        /**
+    	 * Getter of the previous node
+    	 * @return previous node
+    	 */
         public Node getPrev() {
             return prev;
         }
-
+        /**
+    	 * Setter of the previous node
+    	 * @param prev previous node
+    	 */
         public void setPrev(Node prev) {
             this.prev = prev;
         }
-
+        /**
+    	 * Getter of the next node
+    	 * @return next node
+    	 */
         public Node getNext() {
             return next;
         }
-
+        /**
+    	 * Setter of the next node
+    	 * @param next node
+    	 */
         public void setNext(Node next) {
             this.next = next;
         }
