242740e337d264f8b1d9daea2fd90e9642e9e92e
moody16 <jan.bargiel96@gmail.com>
2018-05-17 13:21:06 +0200

    3.1.2 Delegacja metod do obiektu IntArrayStack

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
@@ -2,6 +2,30 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRyhmer {
 
+    public int getTotal() {
+        return intArrayStack.getTotal();
+    }
+
+    public void countIn(int in) {
+        intArrayStack.countIn(in);
+    }
+
+    public boolean callCheck() {
+        return intArrayStack.callCheck();
+    }
+
+    public boolean isFull() {
+        return intArrayStack.isFull();
+    }
+
+    public int peekaboo() {
+        return intArrayStack.peekaboo();
+    }
+
+    public int countOut() {
+        return intArrayStack.countOut();
+    }
+
     IntArrayStack intArrayStack = new IntArrayStack();
 
     public DefaultCountingOutRyhmer(IntArrayStack intArrayStack) {
