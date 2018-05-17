7384db3a868836d55fe9975d977f78b83edf1c6c
Marcin Dzikowski <dzikum@gmail.com>
2018-05-17 13:00:03 +0200

    added Override to HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,10 +4,10 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	int totalRejected = 0;
 
-	public int reportRejected() {
+	private int reportRejected() {
 		return totalRejected;
 	}
-
+	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
 			totalRejected++;
