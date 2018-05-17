0fccf896a8fe20494a224025b105b5a840ecaa4c
Mrucznik <mrucznix@gmail.com>
2018-05-17 18:17:43 +0200

    3.3. Punkt 2 - Dokumentacja

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,24 @@
 package edu.kis.vh.nursery;
 
+/**
+ * @author Mrucznik
+ * Klasa zawierająca implementacje wyliczanki typu Hanoi
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	private int totalRejected = 0;
 
+	/**
+	 * @return ilość odrzuconych liczb
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * Funkcja uruchamiajaca wyliczankę
+	 * @param in ilość liczb do wyliczenia
+	 */
 	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
