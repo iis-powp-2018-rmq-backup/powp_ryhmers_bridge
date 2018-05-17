0a9fc4127163c616628f6698f3d9088c62307a8b
Mrucznik <mrucznix@gmail.com>
2018-05-17 13:12:37 +0200

    Punkt 4 - zmiana nazw

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,18 +1,18 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+	public DefaultCountingOutRhymer defaultCountingOutRyhmer = new DefaultCountingOutRhymer();
 
 	@Override
 	public int countOut() {
 		while (!callCheck())
-			temp.countIn(super.countOut());
+			defaultCountingOutRyhmer.countIn(super.countOut());
 
-		int ret = temp.countOut();
+		int ret = defaultCountingOutRyhmer.countOut();
 
-		while (!temp.callCheck())
-			countIn(temp.countOut());
+		while (!defaultCountingOutRyhmer.callCheck())
+			countIn(defaultCountingOutRyhmer.countOut());
 
 		return ret;
 	}
