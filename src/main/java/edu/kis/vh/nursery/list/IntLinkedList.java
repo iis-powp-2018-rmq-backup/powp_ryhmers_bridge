6b7e9fca6c4d3340221472e25e035f7a7a70ff47
Kamil Krupa <kamilkrupa96@gmail.com>
2018-05-17 14:02:27 +0200

    Change public Node class into IntLinkedList's private nested class (it's used only inside IntLinkedList so it doesn't have to be separate public class)

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -40,4 +40,40 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	private class Node {
+
+		// W klasie IntLinkedList bezpośrednie referencje do pól zostały zamienione na gettery i settery.
+		// Zad. 9 - w całym projekcie nie ma nieużywanych setterów
+		private int value;
+		private Node prev;
+		private Node next;
+
+		Node(int i) {
+			setValue(i);
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
 }
