d99e69f507c5cd10786b79523a9d8591b4324a9c
Maciej Gutenplan <202160@edu.p.lodz.pl>
2018-05-17 12:44:10 +0200

    Bridge 3.1.1 Skopiowana implementacja z DefaultCountingOutRythmer

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,41 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    private static final int emptyStack = -1;
+    private static final int sizeOfStack = 11;
+    private static final int stackCapacity = 12;
+    private int[] NUMBERS = new int[stackCapacity];
+
+    private int total = -1;
+
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
+
+    private int getTotal() {
+        return total;
+    }
+
+    boolean callCheck() {
+        return getTotal() == emptyStack;
+    }
+
+    boolean isFull() {
+        return getTotal() == sizeOfStack;
+    }
+
+    int peekaboo() {
+        if (callCheck())
+            return emptyStack;
+        return NUMBERS[getTotal()];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return emptyStack;
+        return NUMBERS[total--];
+    }
+
+}
