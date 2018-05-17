78813968660e5373882b1599b6daa016c8a306f1
Kamil Krupa <kamilkrupa96@gmail.com>
2018-05-17 14:40:53 +0200

    Change visibility of class constant LIST_EMPTY

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
-	private static final int LIST_EMPTY = -1;
+	public static final int LIST_EMPTY = -1;
 
 	private Node last;
 	private int i;
