f03770857a02dbfc582c1a86f067fa9bf0984bb8
202295 <202295@edu.p.lodz.pl>
2018-05-17 14:56:12 +0200

    Punkt 3

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,23 @@
+
 package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
 	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	
+
 	@Override
 	public int countOut() {
 		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
+
+			temp.countIn(super.countOut());
+
 		int ret = temp.countOut();
-		
+
 		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
+
+			countIn(temp.countOut());
+
 		return ret;
 	}
 }
+//Ostatnia edycja pliku i miejsce z pomocą alt+->
