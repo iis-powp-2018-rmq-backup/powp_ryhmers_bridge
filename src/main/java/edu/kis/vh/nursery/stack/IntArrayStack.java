d40ebfa9a08f88056ad7ab777d135a8e582cc4b3
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-05-17 12:41:50 +0200

    p.1 Dodana implementacja do klasy IntArrayStack (przekopiowana)

--- src/main/java/edu/kis/vh/nursery/stack/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/stack/IntArrayStack.java
@@ -1,5 +1,41 @@
 package edu.kis.vh.nursery.stack;
 
 public class IntArrayStack {
+	private static final int EMPTY_VALUE = -1;
+
+	private static final int MAX_CAPACITy = 12;
+
+	private final int[] numbers = new int[MAX_CAPACITy]; //12 raczej nie nazywac SIZE !!! raczej capacity !!!
+
+	private int total = EMPTY_VALUE; //value if empty 
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	protected boolean callCheck() {
+		return total == EMPTY_VALUE;
+	}
+
+	protected boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return EMPTY_VALUE;
+		return numbers[total];
+	}
+
+	protected int countOut() {
+		if (callCheck())
+			return EMPTY_VALUE;
+		return numbers[total--];
+	}
 
 }
