7bc01d553d4cc781ed7ee630734556cf50ca86a3
Cezary Witkowski <czarecoo@gmail.com>
2018-05-17 12:53:30 +0200

    3.1.2 + 3.1.3 added comment to Rhymersfactory class

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -2,14 +2,15 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRhymer;
 
-	public interface Rhymersfactory {
-	
-		public defaultCountingOutRhymer GetStandardRhymer();
-		
-		public defaultCountingOutRhymer GetFalseRhymer();
-		
-		public defaultCountingOutRhymer GetFIFORhymer();
-		
-		public defaultCountingOutRhymer GetHanoiRhymer();
-		
-	}
+public interface Rhymersfactory {
+
+    public defaultCountingOutRhymer GetStandardRhymer();
+
+    public defaultCountingOutRhymer GetFalseRhymer();
+
+    public defaultCountingOutRhymer GetFIFORhymer();
+
+    public defaultCountingOutRhymer GetHanoiRhymer();
+
+    // Alt + <- i alt + -> pozwalają przechodzic pomiedzy ostatnio modyfikowanymi klasami
+}
