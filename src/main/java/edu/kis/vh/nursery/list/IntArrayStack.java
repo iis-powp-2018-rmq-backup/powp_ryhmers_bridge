1d979fedb59a3ac0a741bd06ff1672e5e2c32c5d
Klaudia Goczal <klaudia@lodz.pl>
2018-05-17 12:34:58 +0200

    3.1.1 Added IntArrayStack

---
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -0,0 +1,39 @@
+package edu.kis.vh.nursery.list;
+
+public class IntArrayStack {
+
+    private static final int CAPACITY = 12;
+    private static final int IS_EMPTY = -1;
+    private final int[] numbers = new int[CAPACITY];
+
+    private int total = IS_EMPTY;
+
+    public void countIn(final int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == IS_EMPTY;
+    }
+
+    public boolean isFull() {
+        return total == CAPACITY - 1;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return IS_EMPTY;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return IS_EMPTY;
+        return numbers[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
+}
