c8da7762dfdae2ed22bf893022ecabbe50ba1f2c
202295 <202295@edu.p.lodz.pl>
2018-05-17 15:05:59 +0200

    P5 extract constant

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int initialtotal = -1;
 
-	public int total = -1;
+	private static final int arraysize = 12;
+
+	private int[] numbers = new int[arraysize];
+
+	public int total = initialtotal;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,7 +16,7 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == initialtotal;
 	}
 
 	public boolean isFull() {
@@ -21,13 +25,13 @@ public class DefaultCountingOutRhymer {
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return initialtotal;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return initialtotal;
 		return numbers[total--];
 	}
 
