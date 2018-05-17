2f212d67f606f0df99464241104fd6461f27ddc7
Agherolab <AgheroLab@mail.com>
2018-05-17 13:08:45 +0200

    IntArrayStack

--- src/main/java/edu/kis/vh/nursery/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -1,11 +1,9 @@
 package edu.kis.vh.nursery;
 
-public class DefaultCountingOutRhymer {
-
+public class IntArrayStack {
     private static final int IS_EMPTY_TOTAl = -1;
     private static final int CAPACITY = 11;
     private final int[] NUMBERS = new int[12];
-
 
     private int total = IS_EMPTY_TOTAl;
 
@@ -33,7 +31,5 @@ public class DefaultCountingOutRhymer {
             return IS_EMPTY_TOTAl;
         return NUMBERS[total--];
     }
-
-
 
 }
