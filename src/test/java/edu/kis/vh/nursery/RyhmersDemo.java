2d7e6fb4c0ec4172643a583dae4acc2a9f28158e
Kamil Krupa <kamilkrupa96@gmail.com>
2018-05-17 14:11:35 +0200

    Extract code from main to separate method

--- src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RyhmersDemo.java
@@ -7,27 +7,33 @@ class RyhmersDemo {
 
 	public static void main(String[] args) {
 		Ryhmersfactory factory = new DefaultRyhmersFactory();
+		rhymersTest(factory);
 
-		DefaultCountingOutRyhmer[] ryhmers = { factory.getStandardRyhmer(), factory.getFalseRyhmer(),
+	}
+
+	private static void rhymersTest(Ryhmersfactory factory) {
+		DefaultCountingOutRyhmer[] ryhmers = {factory.getStandardRyhmer(), factory.getFalseRyhmer(),
 				factory.getFIFORyhmer(), factory.getHanoiRyhmer()};
 
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
+		for (int i = 1; i < 15; i++) {
+			for (int j = 0; j < 3; j++) {
 				ryhmers[j].countIn(i);
+			}
+		}
 
 		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < 15; i++)
+		for (int i = 1; i < 15; i++) {
 			ryhmers[3].countIn(rn.nextInt(20));
+		}
 
 		for (int i = 0; i < ryhmers.length; i++) {
-			while (!ryhmers[i].callCheck())
+			while (!ryhmers[i].callCheck()) {
 				System.out.print(ryhmers[i].countOut() + "  ");
+			}
 			System.out.println();
 		}
 
-		System.out.println("total rejected is "
-				+ ((HanoiRyhmer) ryhmers[3]).reportRejected());
-
+		System.out.println("total rejected is " + ((HanoiRyhmer) ryhmers[3]).reportRejected());
 	}
 
 }
