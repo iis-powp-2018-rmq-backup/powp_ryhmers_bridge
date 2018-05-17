a81db7d01f84343db247a4c22e5000ce01f28c91
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-05-17 15:21:20 +0200

    Enkapsulacja publicznych atrybutow klas. Skomentowanie zachowania automatycznej enkapsulacji w klasie Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,39 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-	public int value;
-	public Node prev, next;
+	private int value;
+	private Node prev;
+	private Node next;
 
+//	Enkapsulacja pol value, prev oraz next spowodowaly automatyczne wygenerowanie setterow i getterow dla tych pol, oraz zmiane
+//	dostepu na private. W klasie IntLinkedList zostaly rowniez dokonane zmiany - poprzednio odwolania do pol byly bezposrednie 
+//	(poniewaz pola byly publiczne), a po enkapsulacji zostaly zamienione na uzycia getterow i setterow do odwolywania sie do pol klasy "Node"
 	public Node(int i) {
 		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
 	}
 	
 }
