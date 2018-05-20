6fd360f8c03aff298ee9101995b4d99127c2119f
mjakmiyo <jedrzejekmonika@gmail.com>
2018-05-20 21:39:11 +0200

    3.1.13 Organize RhymersDemo and add testRhymers method

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,7 +1,5 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.DefaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
@@ -9,27 +7,34 @@ class RhymersDemo {
 
 	public static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
-		
+
+		testRhymers(factory);
+
+	}
+
+	private static void testRhymers(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] ryhmers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-				factory.getFIFORhymer(), factory.getHanoiRhymer()};
-		
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
+				factory.getFIFORhymer(), factory.getHanoiRhymer() };
+
+		for (int i = 1; i < 15; i++) {
+			for (int j = 0; j < 3; j++) {
 				ryhmers[j].countIn(i);
-		
+			}
+		}
+
 		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < 15; i++)
+		for (int i = 1; i < 15; i++) {
 			ryhmers[3].countIn(rn.nextInt(20));
-		
+		}
+
 		for (int i = 0; i < ryhmers.length; i++) {
-			while (!ryhmers[i].callCheck())
+			while (!ryhmers[i].callCheck()) {
 				System.out.print(ryhmers[i].countOut() + "  ");
+			}
 			System.out.println();
 		}
-		
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) ryhmers[3]).reportRejected());
-		
+
+		System.out.println("total rejected is " + ((HanoiRhymer) ryhmers[3]).reportRejected());
 	}
-	
+
 }
