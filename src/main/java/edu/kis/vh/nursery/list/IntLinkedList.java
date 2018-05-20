11ce408e371424580f18587bead5a1cfae104a4f
mjakmiyo <jedrzejekmonika@gmail.com>
2018-05-20 21:17:06 +0200

    3.1.11 + 3.1.12 Change Node.class into IntLinkedList's nested class, because it is used only in IntLinkedList and it doesn't require to be seprated.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -39,4 +39,42 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	private class Node {
+
+		private final int value;
+		private Node prev;
+		private Node next;
+
+		/*
+		 * Powstały gettery i settery do wartości value, prev oraz next. W klasie
+		 * IntLinkedList zmieniło się na: last.next =new Node(i); => last.setNext(new
+		 * Node(i)); last.next.prev = last; => last.getNext().setPrev(last); last =
+		 * last.next; => last = last.getNext();
+		 */
+		protected Node(final int i) {
+			value = i;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(final Node next) {
+			this.next = next;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(final Node prev) {
+			this.prev = prev;
+		}
+
+	}
+
 }
