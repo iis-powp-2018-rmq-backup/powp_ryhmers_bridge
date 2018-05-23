fae060bbc6187d062ed010b41362a6eabef59507
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-05-23 20:28:50 +0200

    Zmieniono atrybut temp na IntArrayStack

--- src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
@@ -1,5 +1,6 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntArrayStack;
 import edu.kis.vh.nursery.stack.IntStackInterface;
 
 public class FIFORyhmer extends DefaultCountingOutRyhmer {
@@ -11,19 +12,17 @@ public class FIFORyhmer extends DefaultCountingOutRyhmer {
 		super(intLinkedList);
 	}
 
-	private final DefaultCountingOutRyhmer temp = new DefaultCountingOutRyhmer();
+	private final IntStackInterface temp = new IntArrayStack();
 
 	@Override
 	public int countOut() {		
 		while (!callCheck())
-			temp.countIn(super.countOut());
-		int ret = temp.countOut();
-		while (!temp.callCheck())
-			countIn(temp.countOut());
+			temp.push(super.countOut());
+		int ret = temp.pop();
+		while (!temp.isEmpty())
+			countIn(temp.pop());
 		return ret;
 	}
-
-	public DefaultCountingOutRyhmer getTemp() {
-		return temp;
-	}
 }
+
+//IntArrayStack to lepszy wybór ponieważ w temp potrzebujemy jedynie ostatniej wartości więc rezerwujemy mniej pamięci
