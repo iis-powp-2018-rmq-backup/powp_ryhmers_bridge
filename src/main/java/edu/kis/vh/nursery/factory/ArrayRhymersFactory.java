ffe5a519eca57851f603e47213f9d369ef36d870
[Roksana_Traczyk] <[zakrencona15@wp.pl]>
2018-05-23 22:05:45 +0200

    Implement ArrayRhymersFactory and ListRhymersFactory

---
+++ src/main/java/edu/kis/vh/nursery/factory/ArrayRhymersFactory.java
@@ -0,0 +1,27 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
+public class ArrayRhymersFactory implements Rhymersfactory {
+    @Override
+    public DefaultCountingOutRhymer getStandardRyhmer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRyhmer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORyhmer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRyhmer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+}
