6fac96f21869bd20c7d243aef20bacdf77be03a7
Rafał Jakubowski <fujitsusiemens3@o2.pl>
2018-05-24 01:31:05 +0200

    Changed temp field type

--- src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
@@ -1,10 +1,11 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntArrayStack;
 import edu.kis.vh.nursery.stack.Stackable;
 
 public class FIFORyhmer extends DefaultCountingOutRyhmer {
 
-	private final DefaultCountingOutRyhmer temp = new DefaultCountingOutRyhmer();
+	private final Stackable temp = new IntArrayStack();
 
 	public FIFORyhmer() {
 	}
@@ -17,14 +18,19 @@ public class FIFORyhmer extends DefaultCountingOutRyhmer {
 	public int countOut() {
 		while (!callCheck())
 
-			temp.countIn(super.countOut());
+			temp.push(super.countOut());
 
-		int ret = temp.countOut();
+		int ret = temp.pop();
 
-		while (!temp.callCheck())
+		while (!temp.isEmpty())
 
-			countIn(temp.countOut());
+			countIn(temp.pop());
 
 		return ret;
 	}
+	
+	/*
+	 * Najlepszym wyborem jest IntArrayStack, ponieważ potrzebujemy 
+	 * ostatniej wartości a tablica wymaga mniej zasobów.
+	 */
 }
