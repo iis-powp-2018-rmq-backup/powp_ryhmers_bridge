34471d64198fd6ae6aa1ecca4ef3203e16cc03f9
adinowi <adinowi@gmail.com>
2018-05-23 21:55:18 +0200

    optymalizacja FIFORyhmer

--- src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
@@ -1,10 +1,12 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntArrayStack;
 import edu.kis.vh.nursery.stack.Stack;
 
 public class FIFORyhmer extends DefaultCountingOutRyhmer {
 
-	private final DefaultCountingOutRyhmer temp = new DefaultCountingOutRyhmer();
+	private final IntArrayStack temp = new IntArrayStack(); //IntArrayStack bedzie lepszy bo bedzie zajmował mniej miejsca
+															//gdyż sklada sie tylko z tablicy , a IntLinkedList dla kazdego argumentu zawiera wartosc oraz wskaik na wczenisjeszy i kolejny elemnet
 	
 	public FIFORyhmer() {
 		super();
@@ -18,13 +20,13 @@ public class FIFORyhmer extends DefaultCountingOutRyhmer {
 	public int countOut() {
 		while (!callCheck())
 			
-			temp.countIn(super.countOut());
+			temp.push(super.countOut());
 		
-		final int ret = temp.countOut();
+		final int ret = temp.pop();
 		
-		while (!temp.callCheck())
+		while (!temp.isEmpty())
 			
-			countIn(temp.countOut());
+			countIn(temp.pop());
 		
 		return ret;
 	}
