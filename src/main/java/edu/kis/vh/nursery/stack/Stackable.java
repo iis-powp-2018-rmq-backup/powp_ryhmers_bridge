e4b3b153cf1049e2896224fd3144846f43b9ffbc
Rafał Jakubowski <fujitsusiemens3@o2.pl>
2018-05-24 01:21:22 +0200

    Compared navigations

--- src/main/java/edu/kis/vh/nursery/stack/Stackable.java
+++ src/main/java/edu/kis/vh/nursery/stack/Stackable.java
@@ -3,8 +3,12 @@ package edu.kis.vh.nursery.stack;
 public interface Stackable {
 
 	int IS_EMPTY = 0;
-	/*Tak, realizacja zadania 10 sprawiła, że wystarczyło zmienić 
+	/* Tak, realizacja zadania 10 sprawiła, że wystarczyło zmienić 
 	 * wartość jednej zmiennej.
+	 * 
+	 * Navigate->Open Declaration - wskazuje implementację w bieżącym pliku 
+	 * Navigate->Quick Type Hierarchy - pokazuje hierarchię klas, pozwala otworzyć implementację w klasie bazowej
+	 * ctrl+mysz - pozwala otworzyć deklarację, implementację oraz implementację w klasie bazowej
 	 */
 
 	int getSize();
