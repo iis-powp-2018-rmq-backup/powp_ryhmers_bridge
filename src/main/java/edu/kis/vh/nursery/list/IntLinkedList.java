227cb7d13c56977a3fd74dad51ee0e548be2b408
ganymede95 <195060@edu.p.lodz.pl>
2018-05-23 16:31:05 +0200

    3.1.12
    Node.java class was a part of linked list used in this program, but aside from that, it was used nowhere else. It could safely be "integrated" into the IntLinkedList.java file and its original file could safely be deleted.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -58,3 +58,39 @@ public class IntLinkedList {
 	dostęp do obiektu Node oraz zmiennej last jest teraz możliwy wyłącznie poprzez gettery i settery
 	 */
 }
+
+class Node {
+
+	private final int value;
+	private Node prev;
+	private Node next;
+
+	public Node(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	/*
+	dostęp do obiektów Node jest teraz możliwy wyłącznie poprzez gettery i settery
+	 */
+}
+
