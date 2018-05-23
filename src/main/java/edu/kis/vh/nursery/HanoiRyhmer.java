6236614805255bd5b7e834f9529c6b340ae54c6d
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-05-23 19:15:13 +0200

    Zad 13 i 14.  Ctr + t pokazuje wszystkie implementacje

--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -1,6 +1,18 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.collections.intType.IntContainer;
+
 public class HanoiRyhmer extends DefaultCountingOutRyhmer {
+
+	public HanoiRyhmer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HanoiRyhmer(IntContainer arrayStack) {
+		super(arrayStack);
+		// TODO Auto-generated constructor stub
+	}
 
 	private int totalRejected = 0;
 
