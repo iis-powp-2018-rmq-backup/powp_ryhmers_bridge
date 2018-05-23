0dba11e60ab039e128f4b86ab68e565a6dc3ad2f
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-05-23 20:15:13 +0200

    Zmieniono DEFAULT_EMPTY_STACK_VALUE na 0 z -1

--- src/main/java/edu/kis/vh/nursery/stack/IntStackInterface.java
+++ src/main/java/edu/kis/vh/nursery/stack/IntStackInterface.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery.stack;
 
 public interface IntStackInterface {
 	
-	final int DEFAULT_EMPTY_STACK_VALUE = -1;
+	final int DEFAULT_EMPTY_STACK_VALUE = 0;
+	//Owszem, realizacja zadania 10 pomogła, dzieki temu należało dokonać zmiany tylko w intefejsie, a nie w obu klasach
 	
 	void push(int in);
 
