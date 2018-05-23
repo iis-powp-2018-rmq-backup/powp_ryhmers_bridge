9a3752a39aa7e9a8bf84534e31478ac6411b61bf
Aleksandra Świderska <olaswiderska95@gmail.com>
2018-05-23 19:10:16 +0200

    Zad 11

--- src/main/java/edu/kis/vh/nursery/collections/intType/IntContainer.java
+++ src/main/java/edu/kis/vh/nursery/collections/intType/IntContainer.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery.collections.intType;
 
 public interface IntContainer {
-	int EMPTY_VALUE = -1;
+	int EMPTY_VALUE = 0; //Zmiana wartosci dla wspolnej stalej jest szybka i bezpieczna. Zad 10 pomoglo
 	int getTotal();
 
 	void countIn(int in);
