4c4025bec805c04b5d7dfad4b9ff2c8110536620
Cezary Witkowski <czarecoo@gmail.com>
2018-05-24 15:02:49 +0200

    changed peekaboo method to protected

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,40 +2,38 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private IntArrayStack stack;
-	
-	public DefaultCountingOutRhymer() {
-		this.stack = new IntArrayStack();
-	}
-	
-	public DefaultCountingOutRhymer(IntArrayStack stack) {
-		this.stack = stack;
-	}
-	
-	public int getTotal() {
-		return stack.getTotal();
-	}
+    private IntArrayStack stack;
 
-	public int peekaboo() {
-		return stack.peekaboo();
-	}
+    public DefaultCountingOutRhymer() {
+        this.stack = new IntArrayStack();
+    }
 
-	public void countIn(int in) {
-		stack.countIn(in);
-	}
+    public DefaultCountingOutRhymer(IntArrayStack stack) {
+        this.stack = stack;
+    }
 
-	public boolean callCheck() {
-		return stack.callCheck();
-	}
+    public int getTotal() {
+        return stack.getTotal();
+    }
 
-	public boolean isFull() {
-		return stack.isFull();
-	}
+    protected int peekaboo() {
+        return stack.peekaboo();
+    }
 
-	public int countOut() {
-		return stack.countOut();
-	}
+    public void countIn(int in) {
+        stack.countIn(in);
+    }
 
+    public boolean callCheck() {
+        return stack.callCheck();
+    }
 
+    public boolean isFull() {
+        return stack.isFull();
+    }
+
+    public int countOut() {
+        return stack.countOut();
+    }
 
 }
