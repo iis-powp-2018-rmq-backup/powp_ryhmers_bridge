7de6682c28601239a7bb3ba80109078565940390
Marcin Dzikowski <dzikum@gmail.com>
2018-05-24 21:24:13 +0200

    3.1.12 changed node class to nested class in IntLinkedList, only this class was using Node

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	private static final int DEFAULT_EMPTY_STACK_VALUE = -1;
 	private Node last;
 	private int i;
 
@@ -10,7 +11,7 @@ public class IntLinkedList {
 			last = new Node(i);
 		else {
 			last.setNext(new Node(i));
-			last.getNext().prev = last;
+			last.getNext().setPrev(last);
 			last = last.getNext();
 		}
 	}
@@ -25,32 +26,66 @@ public class IntLinkedList {
 
 	public int top() {
 		if (isEmpty())
-			return -1;
+			return DEFAULT_EMPTY_STACK_VALUE;
 		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return DEFAULT_EMPTY_STACK_VALUE;
 		final int ret = last.getValue();
-		last = last.prev;
+		last = last.getPrev();
 		return ret;
 	}
 
-	Node getLast() {
+	public Node getLast() {
 		return last;
 	}
 
-	void setLast(Node last) {
+	public void setLast(Node last) {
 		this.last = last;
 	}
 
-	int getI() {
+	public int getI() {
 		return i;
 	}
 
-	void setI(int i) {
+	public void setI(int i) {
 		this.i = i;
 	}
 
 }
+
+class Node {
+
+	private final int value;
+	protected Node prev;
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	private Node next;
+
+	public Node(final int i) {
+		value = i;
+	}
+
+	protected int getValue() {
+		return value;
+	}
+
+
+	protected Node getNext() {
+		return next;
+	}
+
+	protected void setNext(Node next) {
+		this.next = next;
+	}
+
+}
+
