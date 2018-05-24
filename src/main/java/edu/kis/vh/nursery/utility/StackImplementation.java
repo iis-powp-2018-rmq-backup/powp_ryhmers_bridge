75e6ab357b9baeb27eba5ec099a633a87ee18bca
Cezary Witkowski <czarecoo@gmail.com>
2018-05-24 16:07:13 +0200

    3.1.13 added answer

--- src/main/java/edu/kis/vh/nursery/utility/StackImplementation.java
+++ src/main/java/edu/kis/vh/nursery/utility/StackImplementation.java
@@ -22,5 +22,15 @@ public interface StackImplementation {
      *
      * Nie, ponieważ aby zachować funkcjonalność stosu tablicowego musiałem wprowadzić poprawki w wielu miejscach
      *
+     *
+     * Ponownie sprawdź działanie opcji Navigate → Open Declaration (F3) na wywołaniach metod w klasie Stack. Porównaj
+     * działanie z opcją Navigate → Quick Type Hierarchy (ctrl+t) oraz naciśniętego ctrl przy pracy kursora myszki.
+     * Wnioski napisz w komentarzu
+     *
+     * skrót F3 przenosi nas do definicje zaznaczenia
+     * 
+     * ctrl-t pokazuje miejsca definicji i implementacji
+     *
+     * ctrl + myszka umozliwia wybranie ,,najechanej" opcji
      */
 }
