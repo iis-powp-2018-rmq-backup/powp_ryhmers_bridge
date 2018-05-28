eba7178b8b0bc026d5a0170feedd3e04a79a9334
Parada <paradowskim94@gmail.com>
2018-05-28 18:40:06 +0200

    added constructors from super classes

--- src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
@@ -1,8 +1,17 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.collection.StackInterface;
+
 public class FIFORyhmer extends DefaultCountingOutRyhmer {
 
 	private DefaultCountingOutRyhmer ryhmer = new DefaultCountingOutRyhmer();
+
+	public FIFORyhmer() {
+	}
+
+	public FIFORyhmer(StackInterface stack) {
+		super(stack);
+	}
 
 	@Override
 	protected int countOut() {
--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -1,11 +1,20 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.collection.StackInterface;
+
 public class HanoiRyhmer extends DefaultCountingOutRyhmer {
 
 	private int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
+	}
+
+	public HanoiRyhmer() {
+	}
+
+	public HanoiRyhmer(StackInterface stack) {
+		super(stack);
 	}
 
 	@Override
