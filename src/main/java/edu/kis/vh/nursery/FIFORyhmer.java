cc8ce8f524667118eadb3d5b79b8d7f54c827e88
Parada <paradowskim94@gmail.com>
2018-05-28 18:49:43 +0200

    implemented new ryhmer factories

--- src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORyhmer.java
@@ -4,27 +4,26 @@ import edu.kis.vh.nursery.collection.StackInterface;
 
 public class FIFORyhmer extends DefaultCountingOutRyhmer {
 
-	private DefaultCountingOutRyhmer ryhmer = new DefaultCountingOutRyhmer();
+    private DefaultCountingOutRyhmer ryhmer = new DefaultCountingOutRyhmer();
 
-	public FIFORyhmer() {
-	}
+    public FIFORyhmer() {
+    }
 
-	public FIFORyhmer(StackInterface stack) {
-		super(stack);
-	}
+    public FIFORyhmer(StackInterface stack) {
+        super(stack);
+    }
 
-	@Override
-	protected int countOut() {
-		while (!ryhmer.callCheck())
-			
-		ryhmer.countIn(super.countOut());
-		
-		final int ret = ryhmer.countOut();
-		
-		while (!ryhmer.callCheck())
+    @Override protected int countOut() {
+        while (!ryhmer.callCheck())
+
+            ryhmer.countIn(super.countOut());
+
+        final int ret = ryhmer.countOut();
+
+        while (!ryhmer.callCheck())
 
             ryhmer.countIn(ryhmer.countOut());
-		
-		return ret;
-	}
+
+        return ret;
+    }
 }
