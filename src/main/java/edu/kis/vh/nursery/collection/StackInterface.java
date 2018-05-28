b149a711495390b9a2a1798e29a824837e4cc018
Parada <paradowskim94@gmail.com>
2018-05-28 18:21:05 +0200

    refactored classes and stacks, added StackInterface

---
+++ src/main/java/edu/kis/vh/nursery/collection/StackInterface.java
@@ -0,0 +1,16 @@
+package edu.kis.vh.nursery.collection;
+
+public interface StackInterface {
+
+    void push(int i);
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int top();
+
+    int pop();
+
+    int getTotal();
+}
