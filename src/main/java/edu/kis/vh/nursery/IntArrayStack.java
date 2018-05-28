2605e9cf915901d9f99034a622f96552952f4dc1
Parada <paradowskim94@gmail.com>
2018-05-28 17:05:15 +0200

    created IntArrayStack Class

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,43 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+        private static final int NUMBERS_CAPACITY = 12;
+
+        private static final int EMPTY_STACK_VALUE = -1;
+
+        private int[] numbers = new int[NUMBERS_CAPACITY];
+
+        private int total = EMPTY_STACK_VALUE;
+
+
+        public int getTotal() {
+            return total;
+        }
+
+        protected void countIn(final int in) {
+            if (!isFull())
+                numbers[++total] = in;
+        }
+
+        protected boolean callCheck() {
+            return total == EMPTY_STACK_VALUE;
+        }
+
+        protected boolean isFull() {
+            return total == NUMBERS_CAPACITY - 1;
+        }
+
+        protected int peekaboo() {
+            if (callCheck())
+                return EMPTY_STACK_VALUE;
+            return numbers[total];
+        }
+
+        protected int countOut() {
+            if (callCheck())
+                return EMPTY_STACK_VALUE;
+            return numbers[total--];
+        }
+
+    }
+
