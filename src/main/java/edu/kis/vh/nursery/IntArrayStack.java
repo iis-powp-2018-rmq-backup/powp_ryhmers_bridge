4010f844c08fb20951e141e09e4d1e5d8cfff617
Mrucznik <mrucznix@gmail.com>
2018-05-31 20:13:08 +0200

    Copy implementation DefaultCountingOutRyhmer to IntArrayStack

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,41 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+	static final int EMPTY = -1;
+	static final int NUMBER_CAPACITY = 12;
+	static final int FULL = 11;
+
+	private final int[] NUMBERS = new int[NUMBER_CAPACITY];
+	private int total = EMPTY;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void countIn(int in) {
+		if (!isFull())
+			NUMBERS[++total] = in;
+	}
+
+	protected boolean callCheck() {
+		return total == EMPTY;
+	}
+
+	public boolean isFull() {
+		return total == FULL;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return EMPTY;
+		return NUMBERS[total];
+	}
+
+	protected int countOut() {
+		if (callCheck())
+			return EMPTY;
+		return NUMBERS[total--];
+	}
+
+}
