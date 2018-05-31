f236e12611233950557937d862233a1109f7d1e6
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-05-31 20:15:36 +0200

    Moved DefaultCountingOutRhymer implementation to new class and rebuilded DefaultCountingOutRhymer class so it delegate operations to new class

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,66 +1,42 @@
 package edu.kis.vh.nursery;
+import edu.kis.vh.nursery.list.*;
 
 
-/**
- * @author Jacek Olszewski
- * Klasa bazowa wyliczanek, na której opierają się bardziej złożone wyliczanki.
- * Sama w sobie jest samodzielną wyliczanką.
- */
 public class DefaultCountingOutRhymer {
+	private IntArrayStack IntArray;
 
-	private static final int STARTINGTOTAL = -1;
+	
+	public DefaultCountingOutRhymer() {
+		super();
+	}
+	
+	public DefaultCountingOutRhymer(IntArrayStack intArray) {
+		super();
+		IntArray = intArray;
+	}
 
-	private static final int ARRAYSIZE = 12;
-
-	private int[] numbers = new int[ARRAYSIZE];
-
-	private int total = STARTINGTOTAL;
-
-	/**
-	 * @return Całkowita ilość liczb w wyliczance.
-	 */
 	public int getTotal() {
-		return total;
+		return IntArray.getTotal();
 	}
 
-	/**
-	 * @param in Liczba do wliczenia w wyliczankę.
-	 */
 	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
+		IntArray.countIn(in);
 	}
 
-	/**
-	 * @return Czy wyliczanka jest pusta.
-	 */
+	public int lastValue() {
+		return IntArray.lastValue();
+	}
+
 	public boolean callCheck() {
-		return total == STARTINGTOTAL;
+		return IntArray.callCheck();
 	}
 
-	/**
-	 * @return Czy wyliczanka jest zapełniona.
-	 */
 	public boolean isFull() {
-		return total == 11;
+		return IntArray.isFull();
 	}
 
-	/**
-	 * @return Ostatnia wartość w wyliczance (lub -1 jeżeli pusta).
-	 */
-	protected int lastValue() {
-		if (callCheck())
-			return STARTINGTOTAL;
-		return numbers[total];
-	}
-
-	/**
-	 * @return "Pozbycie" się ostatniej liczby z wyliczanki oraz zwrócenie "usuniętej" liczby (lub -1 jeżeli nie ma co usunąć).
-	 */
 	public int countOut() {
-		if (callCheck())
-			return STARTINGTOTAL;
-		return numbers[total--];
+		return IntArray.countOut();
 	}
-
+	
 }
---
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -0,0 +1,60 @@
+package edu.kis.vh.nursery.list;
+
+public class IntArrayStack {
+
+	private static final int STARTINGTOTAL = -1;
+
+	private static final int ARRAYSIZE = 12;
+
+	private int[] numbers = new int[ARRAYSIZE];
+
+	private int total = STARTINGTOTAL;
+
+	/**
+	 * @return Całkowita ilość liczb w wyliczance.
+	 */
+	public int getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param in Liczba do wliczenia w wyliczankę.
+	 */
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	/**
+	 * @return Czy wyliczanka jest pusta.
+	 */
+	public boolean callCheck() {
+		return total == STARTINGTOTAL;
+	}
+
+	/**
+	 * @return Czy wyliczanka jest zapełniona.
+	 */
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	/**
+	 * @return Ostatnia wartość w wyliczance (lub -1 jeżeli pusta).
+	 */
+	public int lastValue() {
+		if (callCheck())
+			return STARTINGTOTAL;
+		return numbers[total];
+	}
+
+	/**
+	 * @return "Pozbycie" się ostatniej liczby z wyliczanki oraz zwrócenie "usuniętej" liczby (lub -1 jeżeli nie ma co usunąć).
+	 */
+	public int countOut() {
+		if (callCheck())
+			return STARTINGTOTAL;
+		return numbers[total--];
+	}
+
+}
