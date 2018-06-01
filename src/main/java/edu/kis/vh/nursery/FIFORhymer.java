8d2e1dfa1e9e2b0fc3d1a098f5d2b1975d2e6fab
Marcin Dzikowski <dzikum@gmail.com>
2018-06-01 17:27:49 +0200

    generated construcots, point 3.12

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,17 @@
 package edu.kis.vh.nursery;
+import edu.kis.vh.nursery.collection.Stack;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
 	private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-
+	
+	public FIFORhymer(){
+	
+	}
+	public FIFORhymer(Stack stack) {
+		super(stack);
+		// TODO Auto-generated constructor stub
+	}
 	@Override
 	public int countOut() {
 		while (!callCheck())
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,4 +1,5 @@
 package edu.kis.vh.nursery;
+import edu.kis.vh.nursery.collection.Stack;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
@@ -7,6 +8,15 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 	public int reportRejected() {
 		return totalRejected;
 	}
+	
+	public HanoiRhymer() {
+	}
+
+	public HanoiRhymer(Stack stack) {
+		super(stack);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void countIn(final int in) {
 		if (!callCheck() && in > peekaboo())
