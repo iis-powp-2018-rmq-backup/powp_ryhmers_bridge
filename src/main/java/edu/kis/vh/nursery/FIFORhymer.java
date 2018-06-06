862ab8f3b5ab6709cbe2f540c2c6616090fe8933
Łukasz Okrąglewski <okragly25@gmail.com>
2018-06-07 01:06:13 +0200

    Documentation comments

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,14 @@
 package edu.kis.vh.nursery;
-
+/**
+ *  @author Łukasz Okrąglewski
+ *  @see edu.kis.vh.nursery.CountingOutRhymer
+ */
 public class FIFORhymer extends CountingOutRhymer {
 
     private CountingOutRhymer temp = new CountingOutRhymer();
-
+    /**
+     * @return values from FIFORhymer
+     */
     @Override
     public int countOut() {
         while (!callCheck())
