40d18a71396e7f5c7eaff906a1db439a65bb08ed
Łukasz Okrąglewski <okragly25@gmail.com>
2018-06-07 00:12:53 +0200

    Fields encapsulated

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,21 +2,26 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int empty = -1;
-    Node last;
+    private static final int empty = -1;
+    private Node last;
+    // sa one uzywane poprzez gettery bezposrednio w publicznych metodach klasy
+
+    public static int getEmpty() {
+        return empty;
+    }
 
     public void push(int i) {
-        if (last == null)
-            last = new Node(i);
+        if (getLast() == null)
+            setLast(new Node(i));
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            getLast().setNext(new Node(i));
+            getLast().getNext().setPrev(getLast());
+            setLast(getLast().getNext());
         }
     }
 
     public boolean isEmpty() {
-        return last == null;
+        return getLast() == null;
     }
 
     public boolean isFull() {
@@ -26,16 +31,23 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return empty;
-        return last.value;
+            return getEmpty();
+        return getLast().getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return empty;
-        int ret = last.value;
-        last = last.prev;
+            return getEmpty();
+        int ret = getLast().getValue();
+        setLast(getLast().getPrev());
         return ret;
     }
 
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
 }
