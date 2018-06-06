597f4b43f8fc87ba0a23f41a3fed9ec343d09922
Łukasz Okrąglewski <okragly25@gmail.com>
2018-06-07 00:39:46 +0200

    Class node deleted

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -50,4 +50,42 @@ public class IntLinkedList {
     public void setLast(Node last) {
         this.last = last;
     }
+
+
+
+    private class Node {
+
+        private int value;
+        private Node prev;
+        private  Node next;
+
+        Node(int i) {
+            setValue(i);
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        void setValue(int value) {
+            this.value = value;
+        }
+
+        Node getPrev() {
+            return prev;
+        }
+
+        void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
