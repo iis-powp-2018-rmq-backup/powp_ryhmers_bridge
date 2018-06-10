a3a9942de93b1d53182ad7d62c30a34896844e80
Aghero <tsaghero@gmail.com>
2018-06-10 21:15:08 +0200

    3.1.11 Zmiana zwracanej wartosci w przypadku pustego stosu

--- src/main/java/edu/kis/vh/nursery/stack/IntLinkedListInterface.java
+++ src/main/java/edu/kis/vh/nursery/stack/IntLinkedListInterface.java
@@ -2,7 +2,9 @@ package edu.kis.vh.nursery.stack;
 
 public interface IntLinkedListInterface {
 
-    int IS_EMPTY = -1;
+    int IS_EMPTY = 0;
+
+    //Wczesniej wykonany punk 3.1.10 umozliwil zmiane tylko w jednym miejscu zamiast w dwoch
 
     void push(int i);
 
