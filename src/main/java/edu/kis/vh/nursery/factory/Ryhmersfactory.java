7211026e32191fb08b19de90e18a840315e7e626
Aghero <tsaghero@gmail.com>
2018-06-10 21:38:12 +0200

    3.1.16 Wzorzec Fabryka Abstrakcyjna

--- src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
@@ -2,6 +2,8 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
+// Wzorzec Fabryka Abstrakcyjna
+
 public interface Ryhmersfactory {
 
     DefaultCountingOutRhymer getStandardRhymer();
