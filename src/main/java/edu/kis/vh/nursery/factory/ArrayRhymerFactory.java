aae66768a3b59d4498299133c3f64c98987164f7
Sowa <202295@edu.p.lodz.pl>
2018-06-11 12:58:10 +0200

    Wygenerowanie fabryk list i tablicy

---
+++ src/main/java/edu/kis/vh/nursery/factory/ArrayRhymerFactory.java
@@ -0,0 +1,31 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+
+public class ArrayRhymerFactory implements Rhymersfactory {
+
+	@Override
+	public DefaultCountingOutRhymer GetStandardRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DefaultCountingOutRhymer GetFalseRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DefaultCountingOutRhymer GetFIFORhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DefaultCountingOutRhymer GetHanoiRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
