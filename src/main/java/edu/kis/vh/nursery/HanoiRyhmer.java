048b5c9647d0e46963a19f8456ee5e7861e739ea
ZarembaKrzysztof <ZarembaKrzysztof@outlook.com>
2018-06-16 12:34:12 +0100

    Refaktoryzacja HanoiRyhmer(). Ponownie przeprowadziłem analizę kodu klasy HanoiRyhmer(). Stworzyłem bezparametrowy konstruktor, którego zadaniem jest przypisać zmiennej totalRejected wartość domyślą (wynoszącą 0). Dopisałem również adnotacje @Override w celu jawnego wymuszenia zastąpienia metody z klasy bazowej, a przy tym do sprawdzenia czy metoda wirtualna zostanie bezproblemowo podmieniona (mógłby wystąpić błąd, gdyby pomylono nazwę metody).

--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -5,8 +5,13 @@ import lombok.Getter;
 public class HanoiRyhmer extends DefaultCountingOutRyhmer {
 
 	@Getter
-	private int totalRejected = 0;
+	private int totalRejected;
 
+	public HanoiRyhmer() {
+	    totalRejected = 0;
+    }
+
+    @Override
 	public void countIn(final int in) {
 		if (!callCheck() && in > peekaboo()) {
             totalRejected = totalRejected + 1;
