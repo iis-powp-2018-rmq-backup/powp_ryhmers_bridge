98287eab021ed26d1125e5bfc8890e95870f8ec6
ZarembaKrzysztof <ZarembaKrzysztof@outlook.com>
2018-06-16 12:27:19 +0100

    Refaktoryzacja interfejsu RyhmersFactory. Usunąłem specyfikator public przy nazwach metod zarejestrowanych w interfejsie, ponieważ już z samego założenia interfejs dostarcza wyłącznie informacje o metodach publicznych. Metody prywatne nie są dozwolone – interesuje nas tylko to co może wywołane na zewnątrz (poza implementującym interfejs obiektem).

--- src/main/java/edu/kis/vh/nursery/factory/IRyhmersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/IRyhmersFactory.java
@@ -3,8 +3,8 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRyhmer;
 
 public interface IRyhmersFactory {
-	public DefaultCountingOutRyhmer getStandardRyhmer();
-	public DefaultCountingOutRyhmer getFalseRyhmer();
-	public DefaultCountingOutRyhmer getFIFORyhmer();
-	public DefaultCountingOutRyhmer getHanoiRyhmer();
+	DefaultCountingOutRyhmer getStandardRyhmer();
+	DefaultCountingOutRyhmer getFalseRyhmer();
+	DefaultCountingOutRyhmer getFIFORyhmer();
+	DefaultCountingOutRyhmer getHanoiRyhmer();
 }
