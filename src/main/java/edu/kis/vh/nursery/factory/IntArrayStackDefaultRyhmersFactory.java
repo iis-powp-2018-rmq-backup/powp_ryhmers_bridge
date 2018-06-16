73ba5ae244aa1a0593634a0c3cfa51bb4d5f16b9
ZarembaKrzysztof <ZarembaKrzysztof@outlook.com>
2018-06-16 18:15:41 +0100

    Stworzenie nowych fabryk.

---
+++ src/main/java/edu/kis/vh/nursery/factory/IntArrayStackDefaultRyhmersFactory.java
@@ -0,0 +1,31 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRyhmer;
+import edu.kis.vh.nursery.FIFORyhmer;
+import edu.kis.vh.nursery.HanoiRyhmer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
+public class IntArrayStackDefaultRyhmersFactory implements IRyhmersFactory {
+
+	@Override
+	public DefaultCountingOutRyhmer getStandardRyhmer() {
+		return new DefaultCountingOutRyhmer(new IntArrayStack());
+	}
+
+	@Override
+	public DefaultCountingOutRyhmer getFalseRyhmer() {
+		return new DefaultCountingOutRyhmer(new IntLinkedList());
+	}
+
+	@Override
+	public DefaultCountingOutRyhmer getFIFORyhmer() {
+		return new FIFORyhmer(new IntArrayStack());
+	}
+
+	@Override
+	public DefaultCountingOutRyhmer getHanoiRyhmer() {
+		return new HanoiRyhmer(new IntArrayStack());
+	}
+
+}
---
+++ src/main/java/edu/kis/vh/nursery/factory/IntLinkedListRyhmersFactory.java
@@ -0,0 +1,31 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRyhmer;
+import edu.kis.vh.nursery.FIFORyhmer;
+import edu.kis.vh.nursery.HanoiRyhmer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
+public class IntLinkedListRyhmersFactory implements IRyhmersFactory {
+
+	@Override
+	public DefaultCountingOutRyhmer getStandardRyhmer() {
+		return new DefaultCountingOutRyhmer(new IntLinkedList());
+	}
+
+	@Override
+	public DefaultCountingOutRyhmer getFalseRyhmer() {
+		return new DefaultCountingOutRyhmer(new IntArrayStack());
+	}
+
+	@Override
+	public DefaultCountingOutRyhmer getFIFORyhmer() {
+		return new FIFORyhmer(new IntLinkedList());
+	}
+
+	@Override
+	public DefaultCountingOutRyhmer getHanoiRyhmer() {
+		return new HanoiRyhmer(new IntLinkedList());
+	}
+
+}
