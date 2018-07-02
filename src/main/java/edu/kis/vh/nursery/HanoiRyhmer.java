04df35f83fc656b51270e3985013c726daa3ea20
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-07-02 17:24:13 +0200

    Generated Javadoc

--- src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRyhmer.java
@@ -2,14 +2,26 @@ package edu.kis.vh.nursery;
 
 import javax.xml.bind.annotation.XmlEnumValue;
 
+/**
+ * Hanoi Ryhmer
+ * 
+ * @author Kuba Sawicki
+ */
 public class HanoiRyhmer extends DefaultCountingOutRyhmer {
 
 	private int totalRejected = 0;
 
+	/**
+	 * returns rejected numbers from Ryhmer
+	 * @return
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/* 
+	 * Places given number in Ryhmer
+	 */
 	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
