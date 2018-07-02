1ae82533002cc45bcd720b4d1f60fd47de875f08
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-07-02 16:01:23 +0200

    Added comment on alt+arrows functions

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -8,5 +8,8 @@ public class Node {
 	public Node(int i) {
 		value = i;
 	}
-
+	
 }
+
+// Alt + arrows left and right, skips whole words, instead of letters (at least on Mac)
+// Alt + arrows up and down, moves whole line up and down (or selection)
