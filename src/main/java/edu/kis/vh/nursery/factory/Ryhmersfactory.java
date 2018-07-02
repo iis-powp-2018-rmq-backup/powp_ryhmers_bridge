0e254dace60d5bb9109008bf2f287fd50202a017
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-07-02 19:10:24 +0200

    Added ArrayRymers and LinkedListRyhmers Factory

--- src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Ryhmersfactory.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.DefaultCountingOutRyhmer;
 
-public interface Ryhmersfactory {
+public interface Ryhmersfactory {							// Abstract Factory Pattern
 	DefaultCountingOutRyhmer getStandardRyhmer();
 
 	DefaultCountingOutRyhmer getFalseRyhmer();
