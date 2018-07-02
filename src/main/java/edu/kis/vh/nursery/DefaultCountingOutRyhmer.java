fc6199b9244b71adcc345b19960a1597b175e73a
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-07-02 16:12:01 +0200

    Generated getter for the field total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRyhmer.java
@@ -10,6 +10,10 @@ public class DefaultCountingOutRyhmer {
 
 	public int total = FIXED_INT;
 
+	public int getTotal() {
+		return total;
+	}
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
